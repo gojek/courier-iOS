59ff45f761d54189dd71014a912246a0f376d892
Alfian Losari <alfian.losari@go-jek.com>
2022-04-11 16:23:28 +0800

    Update destination simulator in test.sh

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -4,5 +4,5 @@ set -eo pipefail
 
 xcodebuild -workspace Courier.xcworkspace \
     -scheme CourierTests \
-    -destination platform=iOS\ Simulator,OS=15.0,name=iPhone\ 13 \
+    -destination platform=iOS\ Simulator,OS=15.2,name=iPhone\ 13 \
     clean test | xcpretty
