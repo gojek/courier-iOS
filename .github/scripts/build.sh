4e64ad54939c4161b28e45317b78171e9fa0f7f9
Alfian Losari <alfian.losari@go-jek.com>
2022-04-21 18:35:13 +0800

    Update build.sh

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -6,26 +6,27 @@ echo "Building CourierCore"
 
 xcodebuild -workspace Courier.xcworkspace \
     -scheme CourierCore \
-    -destination "generic/platform=iOS Simulator"
+    -destination "generic/platform=iOS Simulator" \
     -configuration Release 
 
 echo "Building CourierMQTT"
 
 xcodebuild -workspace Courier.xcworkspace \
     -scheme CourierMQTT \
-    -destination "generic/platform=iOS Simulator"
+    -destination "generic/platform=iOS Simulator" \
     -configuration Release 
 
 echo "Building CourierCommonClient"
 
 xcodebuild -workspace Courier.xcworkspace \
     -scheme CourierCommonClient \
-    -destination "generic/platform=iOS Simulator"
+    -destination "generic/platform=iOS Simulator" \
     -configuration Release 
 
 echo "Building CourierProtobuf"
 
 xcodebuild -workspace Courier.xcworkspace \
     -scheme CourierProtobuf \
-    -destination "generic/platform=iOS Simulator"
+    -destination "generic/platform=iOS Simulator" \
     -configuration Release
+    
