69b589fba50df00c546da2c6d4d2c5995b5c558c
Alfian Losari <alfian.losari@go-jek.com>
2022-05-13 13:00:26 +0700

    Add Podspecs

--- .github/scripts/publish.sh
+++ .github/scripts/publish.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
+pod trunk push --allow-warnings master MQTTClientGJ.podspec
 pod trunk push --allow-warnings master CourierCore.podspec
 pod trunk push --allow-warnings master CourierMQTT.podspec
 pod trunk push --allow-warnings master CourierProtobuf.podspec
-pod trunk push --allow-warnings master MQTTClientGJ.podspec
