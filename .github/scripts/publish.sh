17e399ce798dceaf80fcd4f630874e0fd8f02913
Alfian Losari <alfian.losari@go-jek.com>
2022-05-17 12:03:57 +0700

    Update podpsecs

--- .github/scripts/publish.sh
+++ .github/scripts/publish.sh
@@ -4,5 +4,7 @@ set -eo pipefail
 
 pod trunk push --allow-warnings MQTTClientGJ.podspec
 pod trunk push --allow-warnings CourierCore.podspec
+pod repo update
 pod trunk push --allow-warnings CourierMQTT.podspec
+pod repo update
 pod trunk push --allow-warnings CourierProtobuf.podspec
