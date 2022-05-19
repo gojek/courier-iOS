b18f51543dd56959be91befca6f83eeefd84fc07
Alfian Losari <alfian.losari@go-jek.com>
2022-05-19 09:34:38 +0700

    Update workflow script for deploy stage

--- .github/scripts/publish.sh
+++ .github/scripts/publish.sh
@@ -4,7 +4,5 @@ set -eo pipefail
 
 pod trunk push --allow-warnings MQTTClientGJ.podspec
 pod trunk push --allow-warnings CourierCore.podspec
-pod repo update
-pod trunk push --allow-warnings CourierMQTT.podspec
-pod repo update
-pod trunk push --allow-warnings CourierProtobuf.podspec
+pod trunk push --allow-warnings --synchronous CourierMQTT.podspec
+pod trunk push --allow-warnings --synchronous CourierProtobuf.podspec
