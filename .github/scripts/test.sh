3a22159662b2e84cf9f69c81eb28513280c3045f
Anand Verma <anand.verma@gojek.com>
2025-07-15 10:59:21 +0530

    added script to take latest sable version for testing

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+- name: Load Xcode version
+      uses: maxim-lobanov/setup-xcode@v1
+      with:
+        xcode-version: latest-stable
+
+    - name: Install xcpretty
+      run: gem install xcpretty
+
+    - name: Run Unit Tests
+      run: |
 set -eo pipefail
 
 xcodebuild -workspace Courier.xcworkspace \
